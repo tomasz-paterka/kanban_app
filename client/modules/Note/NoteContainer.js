273366c5f68173b1c1766ae8a57dec1bfd7d65fe
Tomasz Paterka <tomekpaterka@gmail.com>
2018-06-08 22:32:46 +0200

    add components, actions and reducers

---
+++ client/modules/Note/NoteContainer.js
@@ -0,0 +1,9 @@
+import { connect } from 'react-redux';
+import Notes from './Notes';
+import * as noteActions from '../Note/NoteActions';
+
+const mapDispatchToProps = {
+  ...noteActions,
+};
+
+export default connect(null, mapDispatchToProps)(Notes);
